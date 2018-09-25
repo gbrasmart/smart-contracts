00af67eb2aae8a112fcb55f09bd94efe6ab21d1d
Abha Mai <maiabha82@gmail.com>
2018-09-25 23:59:10 +0300

    ICO Bucket added

--- migrations/2_deploy_must_platform.js
+++ migrations/2_deploy_must_platform.js
@@ -1,5 +1,5 @@
 // const web3 = require("web3");
-const TokenBucket = artifacts.require("TokenBucket.sol");
+const ICOBucket = artifacts.require("ICOBucket.sol");
 const RasmartToken = artifacts.require("RasmartToken.sol");
 
 module.exports = async function(deployer, network, [owner, minter]) {
@@ -7,10 +7,10 @@ module.exports = async function(deployer, network, [owner, minter]) {
     const rate = 500 * 10e6 * 10e18;
     const size = 500 * 10e6 * 10e18;
     await deployer.deploy(RasmartToken);
-    await deployer.deploy(TokenBucket, RasmartToken.address, size, rate);
+    await deployer.deploy(ICOBucket, RasmartToken.address, size, rate);
 
     const token = await RasmartToken.deployed();
-    const bucket = await TokenBucket.deployed();
+    const bucket = await ICOBucket.deployed();
 
     token.addMinter(bucket.address);
     bucket.addMinter(minter);
